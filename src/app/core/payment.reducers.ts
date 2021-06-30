c82b9650ce1e0b4825cb8a4a08333c03fd0a3678
Edwin Njeru <mailnjeru@gmail.com>
2021-06-30 19:43:14 +0300

    working pattern with un-optimised

--- src/app/core/payment.reducers.ts
+++ src/app/core/payment.reducers.ts
@@ -6,15 +6,16 @@ import {
   paymentsLoadSuccess
 } from "./payment.actions";
 import {Payment} from "../payments/payment.model";
+import {createDefaultLoadable, Loadable, onLoadableError, onLoadableLoad, onLoadableSuccess} from "loadable-state";
 
 export const paymentStateSelectorKey = 'payments';
 
-export interface State {
+export interface State extends Loadable {
   payments: Payment[];
 }
 
-
 export const initialState: State = {
+  ...createDefaultLoadable(),
   payments: []
 }
 
@@ -22,26 +23,30 @@ const _reducer = createReducer(
   initialState,
 
   // todo modify after effects
-  on(loadPayments, state=> state),
+  on(loadPayments, state=> ({
+    ...onLoadableLoad(state)
+  })),
 
   on(paymentsLoadSuccess, (state, {payments}) => ({
-    ...state,
+    ...onLoadableSuccess(state),
     // todo do not duplicate
     payments: [...payments]
   })),
 
-  on(paymentsLoadFailure, state => state),
+  on(paymentsLoadFailure, (state, {error}) => ({
+    ...onLoadableError(state, error)
+  })),
 
   // todo modify after effects to do nothing
-  on(createPayment, state => state),
+  on(createPayment, state => ({...onLoadableLoad(state)})),
 
   // todo modify after effects to add array
   on(createPaymentSuccess, (state, {payment}) => ({
-    ...state,
-    payments: [...state.payments, payment]
+    ...onLoadableSuccess(state),
+    payments: [...onLoadableSuccess(state).payments, payment]
   })),
 
-  on(createPaymentFailure, state => state)
+  on(createPaymentFailure, (state, {error}) => ({...onLoadableError(state, error)}))
 );
 
 export function reducer(state: State, action: Action): State {
